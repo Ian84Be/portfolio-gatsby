5003e9e5036445024f60dd4ba18f5cca0e6d8bfa
Ian Be <ian84be@gmail.com>
2021-08-21 12:47:05 -0500

    use fontsource instead of gatsby google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Ian Belknap - Software Engineer',
   },
-  plugins: [
-    'gatsby-plugin-styled-components',
-    {
-      resolve: 'gatsby-plugin-google-fonts',
-      options: {
-        fonts: ['Montserrat'],
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-styled-components'],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
+import '@fontsource/montserrat';
 
 import { LightContext } from '../context/LightContext';
 import { GradientContainer } from '../styles/Container';
